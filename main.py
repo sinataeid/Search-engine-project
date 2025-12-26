7e13f785cd4fa243da4a2ebac99be724cc03ea38
Sina Taeid <sina.taeid@gmail.com>
2025-12-26 18:16:16 +0000

    Documents added

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+from bs4 import BeautifulSoup
+import glob
+import chardet
+from pathlib import Path
+
+
+
+paths = glob.glob('/home/bachmoney/Desktop/Search-engine-project/Book/Book/*.html')
+
+
